1825c9e4686f512a43d0a04555a27a40f9bea242
alu10211999 <alumar1999@ieselcaminas.org>
2022-09-19 15:37:10 +0200

    Initial commit

---
+++ src/Exercice13.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Exercice13 {
+
+    public static void main(String[] args) {
+        int x,y,z;
+        Scanner input= new Scanner(System.in);
+
+        System.out.println("Enter a number:");
+        x= input.nextInt();
+        System.out.println("Enter a other number:");
+        y= input.nextInt();
+        System.out.println("Enter a last number:");
+        z= input.nextInt();
+
+        if ((x>=y) && (x>=z) ) {
+            System.out.println("The biggest number is: " + x);
+        } else  ((x>=y) && (x<z)){
+            System.out.println("The biggest number is: " + z);
+        } else ((x<y) &&  ())
+    }
+}
